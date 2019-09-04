5d0b76ea626f44aeec2df69bfc8d46298a6828c3
Megan White <mfw247@gmail.com>
2019-09-04 09:51:29 -0700

    added server and index.js

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req,res) => {
+    res.status(200).json({api: 'up'});
+})
+
+
+server.get('/products',(req,res) => {
+
+})
+
+server.get('/clients',(req,res) => {
+
+})
+
+server.get('/suppliers',(req,res) => {
+
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-server.get('/hello',(req,res) => {
+const server = require('./api/server.js');
 
-})
+const port = 8000;
+server.listen(port, () => {console.log(`\n** API on port ${port} **\n`)})
+
 
 
